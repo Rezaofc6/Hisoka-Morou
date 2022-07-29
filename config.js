d666a08a78cbf07e4f9adb96a6312cc73438a529
Rezaofc6 <108604209+Rezaofc6@users.noreply.github.com>
2022-07-29 20:27:42 +0700

    Update config.js

--- config.js
+++ config.js
@@ -18,9 +18,9 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6283167714830','6281382420312','6287721317870','6288292024190']
+global.owner = ['6289606790112','6281515696780']
 global.premium = ['6288292024190']
-global.packname = 'Hisoka Morrow'
+global.packname = 'RezaOfc'
 global.author = 'WhatsApp Bot'
 global.sessionName = 'hisoka'
 global.prefa = ['','!','.','🐦','🐤','🗿']
